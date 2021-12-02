4977e1798820663b152389c0c6064c21f83cc9ee
wsxfvhu <krmiv77@gmail.com>
2021-12-03 00:39:13 +0300

    Добавьте файлы проекта.

---
+++ Zadanie12/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public static class Circle
+        {
+            public static double LengthCircle(int r0)
+            {
+                double l = 2 * Math.PI * r0;
+                return l;
+            }
+            public static double SquareCircle(int r0)
+            {
+                double s = Math.PI * r0 * r0;
+                return s;
+            }
+            public static bool AffiliationCircle(int x, int y, int r0, int x0 = 0, int y0 = 0)
+            {
+                bool affiliation = false;
+                double r = Math.Sqrt(Math.Pow((x - x0), 2) + Math.Pow((y - y0), 2));
+                if (r <= r0)
+                {
+                    affiliation = true;
+                }
+                return affiliation;
+            }
+        }
+    }
+}
---
+++ Zadanie12/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Zadanie12")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Zadanie12")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("43938516-3e34-46db-8b32-ab3edfa0f864")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
